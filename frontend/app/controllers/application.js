fa5c8161f77176bf57aadadaea546f91aa9ae9e8
developerbro <developerbro@gmail.com>
2014-06-06 14:07:20 +0800

    basig login/logout mechanism

---
+++ frontend/app/controllers/application.js
@@ -0,0 +1,15 @@
+export default Ember.ObjectController.extend({
+    actions: {
+        logout: function() {
+            var session = this.get('session');
+            session.setProperties({
+                isLoggedin : false,
+                name       : '',
+                email      : '',
+                avatarURL  : '',
+                authToken  : ''
+            });
+            localStorage.clear();
+        }
+    }
+});
--- frontend/app/routes/login.js
+++ frontend/app/routes/login.js
@@ -1,1 +1,7 @@
-export default Ember.Route.extend({});
+export default Ember.Route.extend({
+    loggedinObserver : function() {
+        if (this.get('session.isLoggedin')) {
+            this.transitionTo('index');
+        }
+    }.observes('session.isLoggedin')
+});
