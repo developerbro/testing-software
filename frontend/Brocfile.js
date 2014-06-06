83cc6c6c98e8c0be29f1d5ff7a8e175d91a94bc8
developerbro <developerbro@gmail.com>
2014-06-06 16:29:01 +0800

    added some bootstrap styling

--- frontend/Brocfile.js
+++ frontend/Brocfile.js
@@ -1,6 +1,8 @@
 /* global require, module */
-
-var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var pickFiles  = require('broccoli-static-compiler');
+var mergeTrees = require('broccoli-merge-trees');
+var uglifyJS   = require('broccoli-uglify-js');
+var EmberApp   = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp({
   name: require('./package.json').name,
@@ -16,6 +18,20 @@ var app = new EmberApp({
 // Use this to add additional libraries to the generated output files.
 app.import('vendor/ember-data/ember-data.js');
 
+// Ember Simple Auth
+app.import('vendor/ember-simple-auth/ember-simple-auth.js');
+
+// bootstrap
+app.import('vendor/bootstrap/dist/js/bootstrap.js');
+app.import('vendor/bootstrap/dist/css/bootstrap.css');
+app.import('vendor/bootstrap/dist/css/bootstrap-theme.css');
+var bootstrapTree = pickFiles('vendor/bootstrap/dist/fonts', {
+    srcDir  : '/',
+    files   : ['**/*'],
+    destDir : '/fonts'
+});
+
+
 // If the library that you are including contains AMD or ES6 modules that
 // you would like to import into your application please specify an
 // object with the list of modules as keys along with the exports of each
@@ -30,5 +46,7 @@ app.import('vendor/ic-ajax/dist/named-amd/main.js', {
   ]
 });
 
+//app           = uglifyJS(app, {});
+//bootstrapTree = uglifyJS(bootstrapTree, {});
 
-module.exports = app.toTree();
+module.exports = mergeTrees([app.toTree(), bootstrapTree]);
