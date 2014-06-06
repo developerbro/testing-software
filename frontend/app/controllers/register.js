c3d49868a90a5aed62dd94ca1ae21d01fc9ea229
developerbro <developerbro@gmail.com>
2014-06-06 18:19:35 +0800

    login/registration form fix

--- frontend/app/controllers/register.js
+++ frontend/app/controllers/register.js
@@ -16,17 +16,21 @@ export default Ember.ObjectController.extend({
             Em.$.ajax({type: "POST", contentType: "application/json; charset=utf-8", dataType: "json",
                 url: "api/v1/auth/register", data: JSON.stringify(u),
                 success: function (data) {
-                    self.setProperties({'username':'','useremail':'','userpass':'','userpassconfirm':''});
-                    self.transitionToRoute('login');
                 },
                 error: function(response) {
                     if (response.status===422) {
                         self.setProperties({'username':'','useremail':'','userpass':'','userpassconfirm':''});
                         Notify.warning('User already registered and try another!');
                     }
+                },
+                statusCode: {
+                    201: function() {
+                        Notify.warning('You have successfully registered!');
+                        self.setProperties({'username':'','useremail':'','userpass':'','userpassconfirm':''});
+                        self.transitionToRoute('login');
+                    }
                 }
             });
-
         }
     }
 });
