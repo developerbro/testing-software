6f1954fcb9ea6a5d0bb8484a4c0612eeea3bbfdf
developerbro <developerbro@gmail.com>
2014-06-06 20:35:39 +0800

    latest

--- frontend/app/components/gravatar-image.js
+++ frontend/app/components/gravatar-image.js
@@ -1,10 +1,10 @@
 export default Ember.Component.extend({
     size         : '200',
-    'data-style' : "",
+    style        : "",
     gravatarURL  : function() {
         return "http://www.gravatar.com/avatar/"+md5(this.get('email'))+"?s="+this.get('size');
     }.property('email', 'size'),
-    style        : function() {
-        return this.get('data-style');
-    }.property('data-style')
+    'data-style' : function() {
+        return this.get('style');
+    }.property('style')
 });
