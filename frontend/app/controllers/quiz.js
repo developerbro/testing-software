de2b98fc69bc859c7c67257f04c9143081e720a9
developerbro <developerbro@gmail.com>
2014-06-07 13:51:45 +0800

    latest

--- frontend/app/controllers/quiz.js
+++ frontend/app/controllers/quiz.js
@@ -15,6 +15,15 @@ export default Ember.ObjectController.extend({
             });
         },
        save: function(params) {
+           var content = this.get('content');
+           if (Em.isEmpty(content.get('title'))) {
+                Notify.alert('title shouldnt be empty!');
+                return;
+           }
+           if (Em.isEmpty(content.get('question'))) {
+                Notify.alert('question shouldnt be empty!');
+                return;
+           }
            this.get('content').save().then(function(quiz) {
                Notify.alert('successfully saved!');
            }, function(reason){
